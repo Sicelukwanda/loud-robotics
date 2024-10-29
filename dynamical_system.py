65848c106c10f4cd769b86944d4674da0d140e61
Sicelukwanda <sicelukwanda.zwane@gmail.com>
2024-10-29 23:44:35 +0000

    Add visualization for angle and angular velocity

--- dynamical_system.py
+++ dynamical_system.py
@@ -1,9 +1,9 @@
 import torch
 import numpy as np
 import math
-
 from matplotlib import pyplot as plt
 from matplotlib.patches import Circle, FancyArrowPatch
+
 plt.switch_backend('tkagg')
 plt.rc('font', family='serif', size=12)
 plt.rc('text', usetex=True)
@@ -26,28 +26,39 @@ class InvertedPendulum:
         self.m = 1.0
         self.l = 1.0
         self.g = 9.8
-       
+
+        # plotting
+        self.radius = 0.1
+        self.scale = 1.5
     
         self.dt = dt
 
-        # Plotting parameters
-        self.radius = 0.1
-        self.scale = 2.0
-
+        # Initialize plot attributes
         self.fig = None
-        self.ax = None
+        self.ax_pendulum = None
+        self.ax_theta = None
+        self.ax_dtheta = None
         self.rod = None
         self.bob = None
         self.title = None
 
+        # Data for plots
+        self.time_data = []
+        self.theta_data = []
+        self.dtheta_data = []
+
         # Turn on interactive plotting
         plt.ion()
-
-        # initialize x
+    
+        # initializer
         self.reset()
 
     def reset(self):
         self.x = torch.tensor([-torch.pi/2.0, 0.0], **self.tensor_args)
+        self.time_elapsed = 0.0
+        self.time_data = [self.time_elapsed]
+        self.theta_data = [self.x[0].item()]
+        self.dtheta_data = [self.x[1].item()]
         return self.x
 
     def _calculate_next_state(self, theta, dtheta, u):
@@ -64,6 +75,13 @@ class InvertedPendulum:
         theta, dtheta = self.x.clone()
         theta_new, dtheta_new = self._calculate_next_state(theta, dtheta, u)
         self.x = torch.tensor([theta_new, dtheta_new], **self.tensor_args)
+        self.time_elapsed += self.dt
+
+        # Update data lists
+        self.time_data.append(self.time_elapsed)
+        self.theta_data.append(theta_new.item())
+        self.dtheta_data.append(dtheta_new.item())
+
         return self.x
 
     def visualize(self, x=None, show_plot=True):
@@ -75,40 +93,71 @@ class InvertedPendulum:
         bx = np.array([0.0, self.l * math.sin(-theta)])
         by = np.array([0.0, self.l * math.cos(-theta)])
         
-        if self.fig is None and self.ax is None:
+        if self.fig is None:
             # Initialize the figure and axes
-            self.fig, self.ax = plt.subplots()
-            self.ax.set_aspect('equal')
-            self.ax.set_xlim(-self.l*1.2, self.l*1.2)
-            self.ax.set_ylim(-self.l*1.2, self.l*1.2)
-            self.ax.grid(True)
+            self.fig = plt.figure(figsize=(10, 6))
+            gs = self.fig.add_gridspec(2, 2)
+            
+            # Pendulum plot
+            self.ax_pendulum = self.fig.add_subplot(gs[:, 0])
+            self.ax_pendulum.set_aspect('equal')
+            self.ax_pendulum.set_xlim(-self.l*1.2, self.l*1.2)
+            self.ax_pendulum.set_ylim(-self.l*1.2, self.l*1.2)
+            self.ax_pendulum.grid(True)
 
             # Draw anchor
             anchor = Circle((0,0), self.radius/self.scale, color='k')
-            self.ax.add_patch(anchor)
+            self.ax_pendulum.add_patch(anchor)
 
             # Initialize rod line with a thicker line width
-            self.rod, = self.ax.plot(bx, by, lw=4, color='k')  # Thick line for rod
+            self.rod, = self.ax_pendulum.plot(bx, by, lw=4, color='k')  # Thick line for rod
 
-            # Initialize bob with gradient coloring
-            # Note: Gradient coloring on patches is complex; here we'll simulate it with a color map
+            # Initialize bob
             self.bob = Circle((bx[-1], by[-1]), self.radius, color='r')  # Bob as circle
-            self.ax.add_patch(self.bob)
+            self.ax_pendulum.add_patch(self.bob)
 
             # Title
-            self.title = self.ax.set_title(f"$\\Theta$:{theta:.2f}   $\\dot{{\\Theta }}$:{dtheta:.2f}")
+            self.title = self.ax_pendulum.set_title(f"Time: {self.time_elapsed:.2f}s  $\\Theta$:{theta:.2f} rad   $\\dot{{\\Theta }}$:{dtheta:.2f} rad/s")
+
+            # Theta plot
+            self.ax_theta = self.fig.add_subplot(gs[0, 1])
+            self.ax_theta.set_title("Angle ($\\Theta$) over Time")
+            self.line_theta, = self.ax_theta.plot(self.time_data, self.theta_data, color='b')
+            self.ax_theta.set_ylabel("$\\Theta$ (rad)")
+            self.ax_theta.set_xlabel("Time (s)")
+            self.ax_theta.grid(True)
+
+            # dTheta plot
+            self.ax_dtheta = self.fig.add_subplot(gs[1, 1])
+            self.ax_dtheta.set_title("Angular Velocity ($\\dot{\\Theta}$) over Time")
+            self.line_dtheta, = self.ax_dtheta.plot(self.time_data, self.dtheta_data, color='g')
+            self.ax_dtheta.set_ylabel("$\\dot{\\Theta}$ (rad/s)")
+            self.ax_dtheta.set_xlabel("Time (s)")
+            self.ax_dtheta.grid(True)
+
+            plt.tight_layout()
         else:
-            # Update rod data
+            # Update pendulum rod and bob
             self.rod.set_data(bx, by)
-            # Update bob position
             self.bob.center = (bx[-1], by[-1])
+
             # Update title
-            self.title.set_text(f"$\\Theta$:{theta:.2f}   $\\dot{{\\Theta }}$:{dtheta:.2f}")
+            self.title.set_text(f"Time: {self.time_elapsed:.2f}s  $\\Theta$:{theta:.2f} rad   $\\dot{{\\Theta }}$:{dtheta:.2f} rad/s")
+
+            # Update theta plot
+            self.line_theta.set_data(self.time_data, self.theta_data)
+            self.ax_theta.relim()
+            self.ax_theta.autoscale_view()
+
+            # Update dtheta plot
+            self.line_dtheta.set_data(self.time_data, self.dtheta_data)
+            self.ax_dtheta.relim()
+            self.ax_dtheta.autoscale_view()
 
         if show_plot:
             plt.draw()
             plt.pause(0.001)  # Pause to update the plot
-        
+
 # Testing the setup with default tensor arguments
 tensor_args={
             'dtype': torch.float32, 
